de32de4939ec252d6fddcc16b360988d805066e9
Dimkysy <diman.nesterov1995@gmail.com>
2023-10-17 19:34:08 +0300

    Stop_gninnipS_My_sdroW

--- 32/Multiples_of_3_or_5.js
+++ 32/Multiples_of_3_or_5.js
@@ -4,9 +4,6 @@ function solution(number) {
 
     let result = 0
 
-    if (number < 0) {
-        return 0
-    }
     for (let i= 1; i< number; i++) {
         if ((i % 3 === 0) || (i % 5 === 0)) {
             result = result + i
